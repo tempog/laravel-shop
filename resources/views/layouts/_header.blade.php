207730e417db162554676807254a6c528699f728
wenlin <wenlin42@icloud.com>
2019-04-24 20:19:58 +0000

    注册与登录

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <!-- Branding Image -->
         <a href="{{ url('/') }}" class="navbar-brand">
-            Laravel-Shop
+            Laravel Shop
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -17,9 +17,30 @@
 
             <!-- Right Side Of Navbar -->
             <ul class="navbar-nav navbar-right">
-                <!-- Authentication Links -->
-                <li class="nav-item"><a href="#" class="nav-link">登录</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">注册</a></li>
+                <!-- 登录注册链接开始 -->
+                @guest
+                <li class="nav-item"><a href="{{ route('login') }}" class="nav-link">登录</a></li>
+                <li class="nav-item"><a href="{{ route('register') }}" class="nav-link">注册</a></li>
+                @else
+                <li class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" role="button"
+                       data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <img src="https://iocaffcdn.phphub.org/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60"
+                             class="img-responsive img-circle" width="30px" height="30px">
+                        {{ Auth::user()->name }}
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a href="#" class="dropdown-item" id="logout"
+                           onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                            退出登录
+                        </a>
+                        <form action="{{ route('logout') }}" id="logout-form" method="POST" style="display:none;">
+                            {{ csrf_field() }}
+                        </form>
+                    </div>
+                </li>
+                @endguest
+                <!-- 登录注册链接结束 -->
             </ul>
         </div>
     </div>
