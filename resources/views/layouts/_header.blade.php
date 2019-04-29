54656a5933d420348a44c8ef3b3ee4028c3ce5b2
wenlin <wenlin42@icloud.com>
2019-04-29 19:29:15 +0000

    收货地址列表

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -30,6 +30,7 @@
                         {{ Auth::user()->name }}
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a href="{{ route('user_addresses.index') }}" class="dropdown-item">收货地址</a>
                         <a href="#" class="dropdown-item" id="logout"
                            onclick="event.preventDefault();document.getElementById('logout-form').submit();">
                             退出登录
