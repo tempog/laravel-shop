7560159a0c2fe8e919c9e82f7c2e96df2e982889
wenlin <wenlin42@icloud.com>
2019-05-07 19:26:04 +0000

    初始化管理后台

--- app/Http/Controllers/UserAddressesController.php
+++ app/Http/Controllers/UserAddressesController.php
@@ -62,7 +62,7 @@ class UserAddressesController extends Controller
     public function destroy(UserAddress $user_address)
     {
         $this->authorize('own', $user_address);
-        
+
         $user_address->delete();
 
         return [];
