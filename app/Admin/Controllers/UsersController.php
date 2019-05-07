fb26e3325e9652f540f406703532ff26701d8e86
wenlin <wenlin42@icloud.com>
2019-05-07 19:40:56 +0000

    管理后台 - 用户列表

---
+++ app/Admin/Controllers/UsersController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\HasResourceActions;
+use Encore\Admin\Grid;
+use Encore\Admin\Layout\Content;
+
+class UsersController extends Controller
+{
+    use HasResourceActions;
+
+    /**
+     * Index interface.
+     *
+     * @param Content $content
+     * @return Content
+     */
+    public function index(Content $content)
+    {
+        return $content
+            ->header('用户列表')
+            ->body($this->grid());
+    }
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new User);
+
+        $grid->id('ID');
+        $grid->name('用户名');
+        $grid->email('邮箱');
+        $grid->email_verified_at('已验证邮箱')->display(function ($value) {
+            return $value ? '是' : '否';
+        });
+        $grid->created_at('注册时间');
+        $grid->actions(function ($actions) {
+            // 不在每一行后面展示查看按钮
+            $actions->disableView();
+            // 不在每一行后面展示删除按钮
+            $actions->disableDelete();
+            // 不在每一行后面展示编辑按钮
+            $actions->disableEdit();
+        });
+
+        $grid->tools(function ($tool) {
+            //
+            $tool->batch(function ($batch) {
+                // 禁用批量删除按钮
+                $batch->disableDelete();
+            });
+        });
+
+        return $grid;
+    }
+
+
+}
