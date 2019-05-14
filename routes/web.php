9ebc8586b00632ec6545d45232049d3b76a69dc7
wenlin <wenlin42@icloud.com>
2019-05-14 19:54:11 +0000

    用户界面 - 收藏商品列表

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,6 @@
 
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
-Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 
 Auth::routes(['verify' => true]);
 
@@ -27,4 +26,7 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
     Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
     Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
+    Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
 });
+
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
