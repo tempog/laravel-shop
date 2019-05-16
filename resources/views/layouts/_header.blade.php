5abdf3de82b462647885ed60960c3733387e0f7a
wenlin <wenlin42@icloud.com>
2019-05-16 00:22:29 +0000

    头部修改

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -22,7 +22,7 @@
                 <li class="nav-item"><a href="{{ route('login') }}" class="nav-link">登录</a></li>
                 <li class="nav-item"><a href="{{ route('register') }}" class="nav-link">注册</a></li>
                 @else
-                <li class="nav-item"><a href="{{ route('cart.index') }}" class="nav-link mt-1"><i class="fa fa-shopping-cart"></i></a></li>
+                <li class="nav-item"><a href="{{ route('cart.index') }}" class="nav-link mt-1"><i class="fa fa-shopping-cart"></i> 购物车 <strong>{{ count(Auth::user()->cartItems()->get()) }}</strong> 件</a></li>
                 <li class="nav-item dropdown">
                     <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" role="button"
                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
