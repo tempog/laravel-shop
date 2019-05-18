c4e876ec95604cd918153410a50e92b319318cec
wenlin <wenlin42@icloud.com>
2019-05-18 02:31:15 +0000

    创建订单

--- app/Models/ProductSku.php
+++ app/Models/ProductSku.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Exceptions\InternalException;
 use Illuminate\Database\Eloquent\Model;
 
 class ProductSku extends Model
@@ -11,5 +12,24 @@ class ProductSku extends Model
     public function product()
     {
         return $this->belongsTo(Product::class);
+    }
+
+    public function decreaseStock($amount)
+    {
+        if ($amount < 0) {
+            throw new InternalException('减库存不可以小于 0');
+        }
+
+        return $this->newQuery()
+            ->where('id', $this->id)
+            ->where('stock', '>=', $amount)->decrement('stock', $amount);
+    }
+
+    public function addStock($amount)
+    {
+        if ($amount < 0) {
+            throw new InternalException('加库存不可以小于 0');
+        }
+        $this->increment('stock', $amount);
     }
 }
