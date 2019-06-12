06783239a6d76a7018f384de67ade3a0e6e96658
wenlin <wenlin42@icloud.com>
2019-06-12 00:58:00 +0000

    管理后台 - 订单列表

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -17,7 +17,7 @@ class Order extends Model
     const SHIP_STATUS_RECEIVED = 'received';
 
     public static $refundStatusMap = [
-        self::REFUND_STATUS_PENDING    => '为退款',
+        self::REFUND_STATUS_PENDING    => '未退款',
         self::REFUND_STATUS_APPLIED    => '已申请退款',
         self::REFUND_STATUS_PROCESSING => '退款中',
         self::REFUND_STATUS_SUCCESS    => '退款成功',
