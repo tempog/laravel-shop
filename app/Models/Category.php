b237fd6df8270b77ce0cc8438fb7f77a345cebc9
wenlin <wenlin42@icloud.com>
2019-08-04 12:04:57 +0000

    初始化类目模型

---
+++ app/Models/Category.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Models;
+
+use function GuzzleHttp\Psr7\uri_for;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $fillable = ['name', 'is_directory', 'level', 'path'];
+    protected $casts = [
+        'is_directory' => 'boolean',
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function (Category $category) {
+            if (is_null($category->parent_id)) {
+                $category->level = 0;
+                $category->path = '-';
+            } else {
+                $category->level = $category->parent->level + 1;
+                $category->path  = $category->parent->path .$category->parent_id .'-';
+            }
+        });
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function children()
+    {
+        return $this->hasMany(Category::class, 'parent_id');
+    }
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+
+    /**
+     * 获取所有祖先类目的 ID 值
+     * @return array
+     */
+    public function getPathIdsAttribute()
+    {
+        return array_filter(explode('-', trim($this->path, '-')));
+    }
+
+    /**
+     * 获取以 - 为分隔的所有祖先类目名称以及当前类目的名称
+     * @return mixed
+     */
+    public function getFullNameAttribute()
+    {
+        return $this->ancestors
+            ->pluck('name')
+            ->push($this->name)
+            ->imploade(' - ');
+    }
+}
