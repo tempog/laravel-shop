b42ab46be132544d00d9bdf89f2904d55ed20687
wenlin <wenlin42@icloud.com>
2019-08-10 07:32:34 +0000

    后台众筹商品管理

--- app/Admin/Controllers/CrowdfundingProductsController.php
+++ app/Admin/Controllers/CrowdfundingProductsController.php
@@ -3,6 +3,7 @@
 namespace App\Admin\Controllers;
 
 use App\Models\Category;
+use App\Models\CrowdfundingProduct;
 use App\Models\Product;
 use App\Http\Controllers\Controller;
 use Encore\Admin\Controllers\HasResourceActions;
@@ -11,76 +12,55 @@ use Encore\Admin\Grid;
 use Encore\Admin\Layout\Content;
 use Encore\Admin\Show;
 
-class ProductsController extends Controller
+class CrowdfundingProductsController extends Controller
 {
     use HasResourceActions;
 
-    /**
-     * Index interface.
-     *
-     * @param Content $content
-     * @return Content
-     */
     public function index(Content $content)
     {
         return $content
-            ->header('商品列表')
+            ->header('众筹商品列表')
             ->body($this->grid());
     }
 
-
-    /**
-     * Edit interface.
-     *
-     * @param mixed $id
-     * @param Content $content
-     * @return Content
-     */
     public function edit($id, Content $content)
     {
         return $content
-            ->header('编辑商品')
+            ->header('编辑众筹商品')
             ->body($this->form()->edit($id));
     }
 
-    /**
-     * Create interface.
-     *
-     * @param Content $content
-     * @return Content
-     */
     public function create(Content $content)
     {
         return $content
-            ->header('创建商品')
+            ->header('创建众筹商品')
             ->body($this->form());
     }
 
-    /**
-     * Make a grid builder.
-     *
-     * @return Grid
-     */
     protected function grid()
     {
         $grid = new Grid(new Product);
-        $grid->model()->where('type', Product::TYPE_NORMAL)->with(['category']);
 
+        $grid->model()->where('type', Product::TYPE_CROWDFUNDING);
         $grid->id('ID')->sortable();
         $grid->title('商品名称');
-        $grid->column('category.name', '类目');
         $grid->on_sale('已上架')->display(function ($value) {
             return $value ? '是' : '否';
         });
         $grid->price('价格');
-        $grid->rating('评分');
-        $grid->sold_count('销量');
-        $grid->review_count('评论数');
+        // 展示众筹相关字段
+        $grid->column('crowdfunding.target_amount', '目标金额');
+        $grid->column('crowdfunding.end_at', '结束时间');
+        $grid->column('crowdfunding.total_amount', '目前金额');
+        $grid->column('crowdfunding.status', '状态')->display(function ($value) {
+            return CrowdfundingProduct::$statusMap[$value];
+        });
 
         $grid->actions(function ($actions) {
             $actions->disableView();
             $actions->disableDelete();
         });
+
         $grid->tools(function ($tools) {
             $tools->batch(function ($batch) {
                 $batch->disableDelete();
@@ -90,40 +70,33 @@ class ProductsController extends Controller
         return $grid;
     }
 
-    /**
-     * Make a form builder.
-     *
-     * @return Form
-     */
     protected function form()
     {
         $form = new Form(new Product);
 
-        $form->hidden('type')->value(Product::TYPE_NORMAL);
-        $form->text('title', '商品名称')->rules('required');
-
+        $form->hidden('type')->value(Product::TYPE_CROWDFUNDING);
+        $form->text('title', '商品名称')->required();
         $form->select('category_id', '类目')->options(function ($id) {
             $category = Category::find($id);
             if ($category) {
                 return [$category->id => $category->full_name];
             }
         })->ajax('/admin/api/categories?is_directory=0');
-        $form->image('image', '封面图片')->rules('required|image');
-        $form->editor('description', '商品描述')->rules('required');
-        $form->radio('on_sale', '上架')->options(['1' => '是', '0' => '否'])->default('0');
+        $form->image('image', '封面图')->rules('required|image');
+        $form->editor('description', '商品描述')->required();
+        $form->text('crowdfunding.target_amount', '众筹目标金额')->rules('required|numeric|min:0.01');
+        $form->datetime('crowdfunding.end_at', '众筹结束时间')->rules('required|date');
 
-        // 直接添加一对多的关联模型
-        $form->hasMany('skus', 'SKU 列表', function (Form\NestedForm $form) {
-            $form->text('title', 'SKU 名称')->rules('required');
-            $form->text('description', 'SKU 描述')->rules('required');
+        $form->hasMany('skus', '商品 SKU', function (Form\NestedForm $form) {
+            $form->text('title', 'SKU 名称')->required();
+            $form->text('description', 'SKU 描述')->required();
             $form->text('price', '单价')->rules('required|numeric|min:0.01');
             $form->text('stock', '剩余库存')->rules('required|integer|min:0');
         });
 
-        // 定义事件回调，当模型即将保存时会触发这个回调
         $form->saving(function (Form $form) {
-            $form->model()->price = collect($form->input('skus'))->where(Form::REMOVE_FLAG_NAME, 0)
-                ->min('price') ?: 0;
+            $form->model()->price = collect($form->input('skus'))
+                ->where(Form::REMOVE_FLAG_NAME, 0)->min('price');
         });
 
         return $form;
--- database/admin.sql
+++ database/admin.sql
@@ -21,7 +21,7 @@
 
 LOCK TABLES `admin_menu` WRITE;
 /*!40000 ALTER TABLE `admin_menu` DISABLE KEYS */;
-INSERT INTO `admin_menu` VALUES (1,0,1,'首页','fa-bar-chart','/',NULL,NULL,'2019-05-07 19:23:48'),(2,0,7,'系统管理','fa-tasks',NULL,NULL,NULL,'2019-08-04 19:07:43'),(3,2,8,'管理员','fa-users','auth/users',NULL,NULL,'2019-08-04 19:07:43'),(4,2,9,'角色','fa-user','auth/roles',NULL,NULL,'2019-08-04 19:07:43'),(5,2,10,'权限','fa-ban','auth/permissions',NULL,NULL,'2019-08-04 19:07:43'),(6,2,11,'菜单','fa-bars','auth/menu',NULL,NULL,'2019-08-04 19:07:43'),(7,2,12,'操作日志','fa-history','auth/logs',NULL,NULL,'2019-08-04 19:07:43'),(8,0,2,'用户管理','fa-users','/users',NULL,'2019-05-07 19:39:49','2019-05-07 19:40:00'),(9,0,4,'商品管理','fa-cubes','/products',NULL,'2019-05-07 20:32:47','2019-08-04 19:07:43'),(10,0,5,'订单管理','fa-rmb','/orders',NULL,'2019-06-12 00:49:49','2019-08-04 19:07:43'),(11,0,6,'优惠劵管理','fa-tags','/coupon_codes',NULL,'2019-07-16 21:48:55','2019-08-04 19:07:43'),(12,0,3,'类目管理','fa-bars','/categories',NULL,'2019-08-04 19:07:29','2019-08-04 19:07:43');
+INSERT INTO `admin_menu` VALUES (1,0,1,'首页','fa-bar-chart','/',NULL,NULL,'2019-05-07 19:23:48'),(2,0,9,'系统管理','fa-tasks',NULL,NULL,NULL,'2019-08-10 07:22:06'),(3,2,10,'管理员','fa-users','auth/users',NULL,NULL,'2019-08-10 07:22:06'),(4,2,11,'角色','fa-user','auth/roles',NULL,NULL,'2019-08-10 07:22:06'),(5,2,12,'权限','fa-ban','auth/permissions',NULL,NULL,'2019-08-10 07:22:06'),(6,2,13,'菜单','fa-bars','auth/menu',NULL,NULL,'2019-08-10 07:22:06'),(7,2,14,'操作日志','fa-history','auth/logs',NULL,NULL,'2019-08-10 07:22:06'),(8,0,2,'用户管理','fa-users','/users',NULL,'2019-05-07 19:39:49','2019-05-07 19:40:00'),(9,0,4,'商品管理','fa-cubes','/products',NULL,'2019-05-07 20:32:47','2019-08-04 19:07:43'),(10,0,7,'订单管理','fa-rmb','/orders',NULL,'2019-06-12 00:49:49','2019-08-10 07:22:06'),(11,0,8,'优惠劵管理','fa-tags','/coupon_codes',NULL,'2019-07-16 21:48:55','2019-08-10 07:22:06'),(12,0,3,'类目管理','fa-bars','/categories',NULL,'2019-08-04 19:07:29','2019-08-04 19:07:43'),(13,9,6,'众筹商品','fa-flag-checkered','/crowdfunding_products',NULL,'2019-08-10 07:21:06','2019-08-10 07:22:06'),(14,9,5,'普通商品','fa-cubes','/products',NULL,'2019-08-10 07:21:57','2019-08-10 07:22:06');
 /*!40000 ALTER TABLE `admin_menu` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -103,4 +103,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-08-04 20:50:27
+-- Dump completed on 2019-08-10  7:31:44
